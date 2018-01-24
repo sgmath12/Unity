f92829a713cff3d00de43d2bc33c8c89357bbd9c
SeungJu <sgmath12@gmail.com>
2018-01-25 01:57:48 +0900

    Modify some functions...

--- Catalan/Assets/Scripts/MouseClick.cs
+++ Catalan/Assets/Scripts/MouseClick.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class MouseClick : MonoBehaviour {
 
+
+	public GameObject grid;
+	private GridScript gridscript;
 	private float timer_start,timer_end;
 	// Use this for initialization
 	void Start(){
 		timer_start = 0f;
 		timer_end = 0f;
+		gridscript = grid.GetComponent<GridScript> ();
 	}
 
-	// Update is called once per frame
-	void Update () {
-		Vector3 new_scale = new Vector3(0,0,0);
 
-		if (Input.GetMouseButtonDown (0)) 
+	void OnMouseOver() {
+		Vector3 new_scale = new Vector3(0,0,0);
+		string temp;
+		if (Input.GetMouseButtonDown (0)) {
 			timer_start = Time.time; // store the time at the beginning of 'click down' 
+		}
 		if (Input.GetMouseButtonUp (0))
 			timer_end = Time.time; //store the time at the beginning of 'click up'
 
@@ -28,11 +33,17 @@ public class MouseClick : MonoBehaviour {
 			new_scale.x = transform.localScale.y;
 			new_scale.y = transform.localScale.x;
 			transform.localScale = new_scale;
-		
+
+			if (gameObject.tag == "Tile_1")
+				gameObject.tag = "Tile_2";
+			else if (gameObject.tag == "Tile_2")
+				gameObject.tag = "Tile_1";
+
+
 			timer_start = 0;
-			timer_end = 0;
+			timer_end = 0; // must initialize timer_start and timer_end. else, block rotate quickly and continuously.
 		}
 
-
 	}
+
 }
